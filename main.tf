1691fe25a522d01a0d9d38f8abbb7ca5f87a8691
Carlos Almeida <c_cesar007@hotmail.com>
2026-08-14 21:24:19 +0100

    last comiit2

--- main.tf
+++ main.tf
@@ -22,3 +22,8 @@ resource "azurerm_network_security_group" "workload" {
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
 }
+
+resource "azurerm_subnet_network_security_group_association" "this" {
+  subnet_id                 = azurerm_subnet.this.id
+  network_security_group_id = azurerm_network_security_group.workload.id
+}
